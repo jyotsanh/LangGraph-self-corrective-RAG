4f5f2b55c743041ad4d92ddfba2b36a3497f7d20
jyotsanh <hamaljyotsan@gmail.com>
2025-01-28 15:27:59 +0545

    update vectore store route works now.

--- src/server.py
+++ src/server.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from routes.chat_routes import router as chat_router
 from routes.update_routes import router as update_router
 
+# logs
+from logs.logger_config import logger
+
 app = FastAPI(
     title="LangGraph based Self-Corrective RAG",
     description="Self-Corrective RAG made using LangGraph"
@@ -12,6 +15,8 @@ app.include_router(chat_router)
 
 app.include_router(update_router)
 
+logger.info("\nFastAPI app initialized successfully.\n")
+
 # Optional: Add health check endpoint
 @app.get("/")
 async def health_check():
@@ -19,3 +24,8 @@ async def health_check():
                     "status": "healthy"
                 }
     return response
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000,reload=True)
